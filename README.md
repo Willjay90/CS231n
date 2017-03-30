a1e0d7e77d5bcecd5866eb6fa6f9b332d9ecc38c
Wei Chieh Tseng <WeiJay@Weis-MacBook-Pro.local>
2017-03-30 17:19:28 +0800

    Softmax
    Softmax Completed

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Assignment 1:
 ```diff
 + Q1: k-Nearest Neighbor classifier (Finished)
 + Q2: Training a Support Vector Machine (Finished)
-- Q3: Implement a Softmax classifier
++ Q3: Implement a Softmax classifier
 - Q4: Two-Layer Neural Network
 - Q5: Higher Level Representations: Image Features
 ```
--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -38,7 +38,7 @@ def softmax_loss_naive(W, X, y, reg):
     probabilities = np.exp(scores) / np.sum(np.exp(scores))
     # loss is the log of the probability of the correct class
     loss += -np.log(probabilities[y[i]])
-    
+
     probabilities[y[i]] -= 1 # calculate p-1 and later we'll put the negative back
 
     # dW is adjusted by each row being the X[i] pixel values by the probability vector
@@ -71,7 +71,7 @@ def softmax_loss_vectorized(W, X, y, reg):
   # Initialize the loss and gradient to zero.
   loss = 0.0
   dW = np.zeros_like(W)
-
+  num_train = X.shape[0]
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
@@ -79,9 +79,20 @@ def softmax_loss_vectorized(W, X, y, reg):
   # regularization!                                                           #
   #############################################################################
   scores = X.dot(W)
+  scores -= np.max(scores, axis=1)[:, None]
+  probabilities = np.exp(scores) / np.sum(np.exp(scores), axis=1)[:, None]
+  loss = np.sum(-np.log(probabilities[np.arange(probabilities.shape[0]), y]))
 
-
-  pass
+  probabilities[np.arange(probabilities.shape[0]), y] -= 1
+  dW += X.T.dot(probabilities)
+  # Right now the loss is a sum over all training examples, but we want it
+  # to be an average instead so we divide by num_train.
+  loss /= num_train
+  dW /= num_train
+        
+  # Add regularization to the loss.
+  loss += 0.5 * reg * np.sum(W * W)
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
